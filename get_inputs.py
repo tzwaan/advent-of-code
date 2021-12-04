7c3e302c3df9c5abbcf4230891219049cdd2a647
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2021-12-04 17:54:23 +0100

    Update get_inputs with progress bars

--- get_inputs.py
+++ get_inputs.py
@@ -1,16 +1,26 @@
 import pathlib
 import datetime
 import aocd
+from tqdm import tqdm
 
+starting_year = 2015
+current_year = datetime.datetime.now().year
 
-for year in range(2015, datetime.datetime.now().year + 1):
-    for day in range(1, 26):
+total_years = starting_year - current_year + 1
+
+for year in tqdm(
+        range(starting_year, current_year + 1),
+        total=total_years,
+        leave=False
+):
+    for day in tqdm(range(1, 26), total=25, leave=False):
+        try:
+            data = aocd.get_data(day=day, year=year)
+        except aocd.exceptions.PuzzleLockedError as e:
+            tqdm.write(str(e))
+            continue
         folder = f'{year}/day{day}'
         pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
         with open(f'{folder}/input.txt', mode='w') as input_file:
-            try:
-                data = aocd.get_data(day=day, year=year)
-                input_file.write(data)
-            except aocd.exceptions.PuzzleLockedError:
-                pass
+            input_file.write(data)
 
